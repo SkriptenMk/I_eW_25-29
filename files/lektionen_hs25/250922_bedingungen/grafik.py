c7970053821339c0e0f9e4c21c8b565ac18838be
Jacques Mock Schindler <jacques.mock@kbw.ch>
2025-09-20 16:56:57 +0200

    Konstruiere Blume mit drei Farben

--- files/lektionen_hs25/250922_bedingungen/grafik.py
+++ files/lektionen_hs25/250922_bedingungen/grafik.py
@@ -30,3 +30,41 @@ zeige_grafik(blume)
 # %%
 speichere_grafik('blumenbeispiel.svg', blume)
 # %%
+
+lavender = rgb_farbe(230, 230, 250)
+mediumpurple = rgb_farbe(147, 112, 219)
+darkorchid = rgb_farbe(153, 50, 204)
+indigo = rgb_farbe(75, 0, 130)
+
+# %%
+
+bluetenblatt_l = ellipse(30, 150, lavender)
+bluetenblatt_m = ellipse(30, 150, mediumpurple)
+bluetenblatt_d = ellipse(30, 150, darkorchid)
+scheibe_i = ellipse(25, 25, indigo)
+
+
+# %%
+bluetenblatt_m_fix = fixiere(unten_mitte, bluetenblatt_m)
+bluetenblatt_l_fix = fixiere(unten_mitte, bluetenblatt_l)
+bluetenblatt_d_fix = fixiere(unten_mitte, bluetenblatt_d)
+blume3 = scheibe_i
+for i in range(24):
+    bm_temp = drehe(i * 15, bluetenblatt_m_fix)
+    bl_temp = drehe(i * 15, bluetenblatt_l_fix)
+    bd_temp = drehe(i * 15, bluetenblatt_d_fix)
+    
+    if i % 4 == 0:
+        blume3 = kombiniere(blume3, bl_temp)
+    elif i % 4 == 2:
+        blume3 = kombiniere(blume3, bd_temp)
+    else:
+        blume3 = kombiniere(blume3, bm_temp)
+        
+zeige_grafik(blume3)
+    
+# %%
+
+speichere_grafik('dreifachblume.svg', blume3)
+
+# %%
